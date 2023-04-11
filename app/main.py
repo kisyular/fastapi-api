599afd7008a4eb5ab43902a509efb502e6899c8c
kisyular <rellika@kisyula.com>
2023-04-11 22:35:26 +0300

    FASTAPI API CRUD OPERATION USING POSTGRESQL AND PYTHON

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from typing import Optional
 from random import randrange
 import psycopg
 import time
-
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -28,7 +27,8 @@ while True:
             port=db_port,
             user=db_user,
             password=db_password,
-            # database="fastapi",
+            dbname="fastapi",
+            row_factory=psycopg.rows.dict_row,
         )
         print("Connected to the database")
         cursor = conn.cursor()
@@ -82,56 +82,82 @@ async def root():
 
 @app.get("/posts", status_code=status.HTTP_200_OK)
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("SELECT * FROM posts ORDER BY id")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute(
+        "INSERT INTO posts (title, content, published, category) VALUES (%s, %s, %s,  %s) RETURNING *",
+        (post.title, post.content, post.published, post.category),
+    )
+    conn.commit()
+    return {"data": post.dict()}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
+    post = cursor.fetchone()
     if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} was not found",
-        )
-    return {"post_detail": post}
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    index = find_index_post(id)
-
-    if index == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exist",
         )
+    return {"data": post}
 
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    # updating post
+    cursor.execute(
+        "UPDATE posts SET title = %s, content = %s, published = %s, category = %s WHERE id = %s RETURNING *",
+        (post.title, post.content, post.published, post.category, id),
+    )
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if not updated_post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+    return {"data": updated_post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     # deleting post
-    # find the index in the array that has required ID
-    # my_posts.pop(index)
-    index = find_index_post(id)
+    cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (id,))
+    deleted_post = cursor.fetchone()
+    conn.commit()
 
-    if index == None:
+    if not deleted_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exist",
         )
 
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+# Get Posts by Category
+@app.get("/posts/category/{category}")
+def get_posts_by_category(category: str):
+    cursor.execute("SELECT * FROM posts WHERE category = %s", (category,))
+    posts = cursor.fetchall()
+    if not posts:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with category: {category} does not exist",
+        )
+    return {"data": posts}
+
+
+# Get Posts by Published
+@app.get("/posts/published/{published}")
+def get_posts_by_published(published: bool):
+    cursor.execute("SELECT * FROM posts WHERE published = %s", (published,))
+    posts = cursor.fetchall()
+    return {"data": posts}
