55feda93147ca59eaa18217f935a2b99d1e494d9
kisyular <rellika@kisyula.com>
2023-04-14 17:27:51 +0300

    FASTAPI API - ADDED ALLOWED ORIGIN TO CORS

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ origins = [
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
